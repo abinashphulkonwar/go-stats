725fb2cf86d5d04732d38382a893f60bc30bf0a5
abinash <abinashphulkonwar1@gmail.com>
2024-07-17 20:45:18 +0530

    add mode func and test

--- src/descriptive/descriptive.go
+++ src/descriptive/descriptive.go
@@ -6,6 +6,9 @@ import (
 	"github.com/abinashphulkonwar/stats/src/services"
 )
 
+/*
+Mean: Σx / n
+*/
 func Mean(values []float64) float64 {
 	var sum float64
 	for _, v := range values {
@@ -13,6 +16,64 @@ func Mean(values []float64) float64 {
 	}
 	sum = sum / float64(len(values))
 	return sum
+}
+
+func Frequency(values []float64, item float64) int {
+	count := 0
+	for _, v := range values {
+		if v == item {
+			count++
+		}
+	}
+	return count
+}
+func Frequencys(values []float64) map[float64]int {
+	f := make(map[float64]int)
+	for _, v := range values {
+		_, ok := f[v]
+		if ok {
+			f[v] += 1
+		} else {
+			f[v] = 1
+		}
+	}
+	return f
+}
+
+/*
+mean = ΣF(x) / ΣF(x)
+*/
+func WidhtedMean(values []float64) float64 {
+	var sum float64
+	f := make(map[float64]int)
+	for _, v := range values {
+		_, ok := f[v]
+		if ok {
+			f[v] += 1
+		} else {
+			f[v] = 1
+		}
+	}
+	count := 0
+	for k, v := range f {
+		sum += k * float64(v)
+		count += v
+	}
+
+	return sum / float64(count)
+}
+
+/*
+portion = ΣF(x i) / n
+*/
+func PortionMean(values []float64, item float64) float64 {
+	var sum float64
+	for _, v := range values {
+		if v == item {
+			sum += v
+		}
+	}
+	return sum / float64(len(values))
 }
 
 func Median(values []float64) float64 {
@@ -34,4 +95,37 @@ func Median(values []float64) float64 {
 	}
 
 	return sortedValues[len(sortedValues)/2]
+}
+
+func Mode(input []float64) []float64 {
+
+	values := services.Sort(input)
+
+	var mode []float64
+
+	count, maxCount := 0, 0
+	for i := 1; i < len(values); i++ {
+		switch {
+		case values[i] == values[i-1]:
+			count++
+		case count > maxCount:
+			mode = append(mode[:0], values[i-1])
+			maxCount = count
+			count = 0
+		case count == maxCount && maxCount != 0:
+			mode = append(mode, values[i-1])
+			count = 0
+		default:
+			count = 0
+		}
+
+	}
+	if maxCount > 0 && count == maxCount {
+		mode = append(mode, values[len(values)-1])
+	} else if maxCount > 0 && count > maxCount {
+		mode = append(mode[:0], values[len(values)-1])
+	}
+
+	return mode
+
 }
--- src/descriptive/descriptive_test.go
+++ src/descriptive/descriptive_test.go
@@ -1,11 +1,23 @@
 package descriptive_test
 
 import (
+	"math"
 	"testing"
 
 	"github.com/abinashphulkonwar/stats/src/descriptive"
 	"github.com/montanaflynn/stats"
 )
+
+func TestWidghtedMean(t *testing.T) {
+	values := []float64{1, 8, 2, 3, 7, 9, 10, 4, 5, 6, 9}
+	statsMean, _ := stats.Mean(values)
+
+	mean := descriptive.WidhtedMean(values)
+	if mean != statsMean {
+		t.Errorf("Expected %v, got %v", statsMean, mean)
+	}
+
+}
 
 func TestMedian(t *testing.T) {
 	values := []float64{1, 8, 2, 3, 7, 9, 10, 4, 5, 6, 9}
@@ -17,4 +29,21 @@ func TestMedian(t *testing.T) {
 		t.Errorf("Expected %v, got %v", statsMedian, median)
 	}
 
+}
+
+func TestMode(t *testing.T) {
+	values := []float64{1, 8, 2, 3, 7, 9, 10, 4, 5, 6, 6, 7, 9, 9}
+	statsMode, _ := stats.Mode(values)
+
+	mode := descriptive.Mode(values)
+	if len(mode) != len(statsMode) {
+		t.Errorf("Expected %v, got %v", statsMode, mode)
+	}
+	max := math.Max(float64(len(statsMode)), float64(len(mode)))
+
+	for i := 0; i < int(max)-1; i++ {
+		if mode[i] != statsMode[i] {
+			t.Errorf("Expected %v, got %v", statsMode, mode)
+		}
+	}
 }
