29031244f85e4d8ac5f159e9e761e484e0ebe0f5
abinash <abinashphulkonwar1@gmail.com>
2024-07-19 09:51:19 +0530

    add percentile func

--- src/descriptive/quantiles.go
+++ src/descriptive/quantiles.go
@@ -11,17 +11,13 @@ type Quantiles struct {
 	Q4 float64
 }
 
-func Position(p int, l int) int {
-	return int((float64(p) / float64(100)) * float64(l-1))
-}
-
 func Quantile(inputs []float64) Quantiles {
 	values := services.Sort(inputs)
 	l := len(values)
-	q1 := values[Position(25, l)]
-	q2 := values[Position(50, l)]
-	q3 := values[Position(75, l)]
-	q4 := values[Position(100, l)]
+	q1 := values[services.Position(25, l)]
+	q2 := values[services.Position(50, l)]
+	q3 := values[services.Position(75, l)]
+	q4 := values[services.Position(100, l)]
 
 	return Quantiles{
 		Q1: q1,
@@ -38,8 +34,20 @@ func Detailed(input []float64) []float64 {
 	l := len(values)
 	detailds := []float64{}
 	for i := 10; i <= 100; i = i + 10 {
-		detailds = append(detailds, values[Position(i, l)])
+		detailds = append(detailds, values[services.Position(i, l)])
 	}
 
 	return detailds
+}
+
+func Percentile(input []float64, percent float64) float64 {
+
+	values := services.Sort(input)
+	i := services.PositionF(percent, len(input))
+	if percent == float64(int(percent)) {
+		return values[i]
+	} else {
+		return (values[i-1] + values[i]) / 2
+
+	}
 }
